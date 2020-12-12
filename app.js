393d8af9b421cb2ffc730be1ce2d0f39952789a2
1styrGroupB <1styrGroupB@pn.local>
2020-12-12 21:55:41 +0800

    Changes 12-12-2020

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 
-const PORT = process.env.PORT || 4000;
-
-
+const PORT = process.env.PORT || 5000;
 
 var express = require('express');
 var app = express();
@@ -15,19 +13,43 @@ app.use('/static',express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
+
+let username = '';
+
 app.get('/',(req,res)=>{
     res.render('index');
 })
 app.get('/home/:username', (req,res)=>{
-    const username = req.params.username;
+    username = req.params.username;
     res.render('home',{username: username});
 })
 
+let activeUser = []
+
+
+function addActive(id,username){
+    let result = activeUser.findIndex(element => element.username == username)
+    if(result == -1){
+        activeUser.push({id:id,username:username})
+    }
+    return activeUser
+}
+
+function leaveUser(id){
+    let result = activeUser.findIndex(element => element.id == id)
+
+    if(result == -1){
+        activeUser.splice(id,1)
+    }
+    return activeUser
+}
+
 //socket io connection
 io.on('connection', (socket) => {
+    socket.broadcast.emit('checkactiveUser', addActive(socket.id,username));
     console.log('a user connected');
 
-    socket.on('join', (msg)=> {
+    socket.on('join', (msg)=> { 
         socket.broadcast.emit('join', msg);
     });
 
@@ -42,6 +64,13 @@ io.on('connection', (socket) => {
     socket.on('disconnection', (msg)=> {
         socket.broadcast.emit('disconnection', msg);
     })
+
+    socket.on('disconnect', function() {
+        console.log(socket.id)
+        socket.broadcast.emit('checkactiveUser', leaveUser(socket.id));
+        console.log(activeUser  )
+        console.log("User disconnect")
+    });
 });
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,128 +1,24 @@
-$(document).ready(function() {
-   $("#sub").click(()=>{
+$(document).ready(function() { 
+    $("#sub").click(()=>{
     let username = getUsername();
         if(username) {
             console.log(username);
             window.location.href = "/home/"+ username;
         }
    });
-
-   $("#discon").click( async (e)=>{
-       e.preventDefault();
-        var socket = io();
-        let user = $("#usernameHome").attr("name");
-       await socket.emit('disconnection', user);
-       let counter = 0;
-       const test = setInterval(()=> {
-            if(counter > 0){
-                window.location.href = '/';
-                clearInterval(test);
-            }
-            counter++;
-
-       },1000)
-    });
-    
-
-   if($("#usernameHome").attr("name")) {
-        var socket = io();
-        let user = $("#usernameHome").attr("name");
-        
-        socket.emit('join', user);
-        displayJoinedChat("you");
-
-        socket.on('join', (msg)=> {
-            //append the user joined  
-            displayJoinedChat(msg);
-        })
-
-        //detect when the a user is typing
-        $("body").on('input',"#msg",()=> {
-            user = $("#usernameHome").attr("name");
-            socket.emit('typing', user);
-        })
-
-        //send the message 
-        $("#send").click((e)=> {
-            let user = $("#usernameHome").attr("name");
-            e.preventDefault();
-            let message = $("#msg").val();
-            socket.emit('message',{username:user, message: message});
-            appendMessage(`${message}`, {direction: "right", color: "primary"});
-            $("#msg").val("");
-        });
-
-        //recieve the emited message form the other users
-        socket.on('message', (msg)=> {
-            appendMessage(`${msg.username} : ${msg.message}`, {direction: "left", color: "success"});
-            clearTyping();
-        })
-
-        //on typing 
-        socket.on('typing', (msg)=> {
-            showTyping(msg);
-        })
-
-        //disconnect
-        socket.on('disconnection', (msg)=> {
-            disConnect(msg);
-            
-        })
-   }
+ 
 })
 
+  /**
+         * get the username of the 
+         */
+        function getUsername() {
+            let username = $("#username").val();
+            if (!username) {
+                alert("Invalid user name");
+                return false;
+            }
+            return username;
+        }
 
-/**
- * Display 
- * @param {*} username 
- */
-function displayJoinedChat(username) {
-    $("#messages").append(
-        `<p class="text-center text-warning"><medium >${username} joined the chat</medium></p>`
-    );
-}
-
-/**
- * Show if a user is typing
- * @param {*} username 
- */
-function showTyping(username){
-    $("#typing").text(`${username} is typing`);
-}
-
-function clearTyping() {
-    $("#typing").text("");
-}
-
-/**
- * Append the message
- * @param {*} message 
- * @param {*} socket 
- */
-function appendMessage(message, data){
-    $("#messages").append(
-        `<div class="mt-2 mx-1"><br><p class="text-${data.direction}"><span class="p-2 bg-${data.color}">${message}</p></span></div>`
-    )
-}
-
-/**
- * get the username of the 
- */
-function  getUsername() {
-    let username = $("#username").val();
-    if(!username) {
-        alert("Invalid user name");
-        return false;
-    }
-    return username;
-}
-
-/**
- * Disconnect
- */
-function disConnect(username) {
-    $("#messages").append(
-        `<p class="text-center text-secondary"><small >${username} has disconnected</small></p>`
-    );
-}
 
